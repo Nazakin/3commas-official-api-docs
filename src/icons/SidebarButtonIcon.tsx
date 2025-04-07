89aaa2e70dce3ef973d0cf7e2f545b2c63a21144
Nazar Kuchynsky <n.kuchynskyi@3commas.io>
2025-04-07 17:23:44 +0200

    BOST-74 feat: fixed styles for sidebar button

--- src/icons/SidebarButtonIcon.tsx
+++ src/icons/SidebarButtonIcon.tsx
@@ -1,5 +1,5 @@
 export const SidebarButtonIcon = () => (
-    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M5.2168 3H19.2168M5.2168 3C4.11223 3 3.2168 3.89543 3.2168 5V19C3.2168 20.1046 4.11223 21 5.2168 21M5.2168 3H9.2168M19.2168 3C20.3214 3 21.2168 3.89543 21.2168 5V19C21.2168 20.1046 20.3214 21 19.2168 21M19.2168 3H9.2168M19.2168 21H5.2168M19.2168 21H9.2168M5.2168 21H9.2168M9.2168 3V21" stroke="#B8B8B8" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" id='sidebarSvg'/>
     </svg>
 
